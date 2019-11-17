83a163dce7f3f3ad4c14ebaefb1e10191c09404e
Artur Vrublevskiy <v8a8rt@gmail.com>
2019-11-17 20:22:35 +0300

    main page done

---
+++ app/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    var mainSlider = $('.main-slider__wrapper');
+    mainSlider.owlCarousel({
+        items:1,
+    });
+
+    $('.main-slider__button--next').click(function() {
+        mainSlider.trigger('next.owl.carousel');
+        console.log('next')
+    })
+    // Go to the previous item
+    $('.customPrevBtn').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        mainSlider.trigger('prev.owl.carousel');
+    })
+    var reviewsSlider = $('.reviews__slider-wrapper');
+    reviewsSlider.owlCarousel({
+        items:1,
+    });
+    $('.main-slider__button--next').click(function() {
+        reviewsSlider.trigger('next.owl.carousel');
+        console.log('next')
+    })
+    // Go to the previous item
+    $('.customPrevBtn').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        reviewsSlider.trigger('prev.owl.carousel');
+    })
+});
